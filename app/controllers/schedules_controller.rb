e63cf470837b74a2a6bde1621fbc5565b7d6f50b
macle128 <macle128@umn.edu>
2025-01-21 09:54:32 -0600

    working on linting

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -7,10 +7,9 @@ class SchedulesController < ApplicationController
   def work_orders
     technician = Technician.find(params[:id])
     work_orders = technician.work_orders # Assuming a has_many association
-    
     render json: work_orders, status: :ok
   rescue ActiveRecord::RecordNotFound
-    render json: { error: 'Technician not found' }, status: :not_found
+    render json: { error: "Technician not found" }, status: :not_found
   end
 
 end
